1f0d957d3e4eabe6d61ff38ba9d9a6593e43015c
Brandon Rainey <brandonhrainey@gmail.com>
2022-02-05 01:43:04 -0500

    label sematics v2

--- components/ChartSelector.js
+++ components/ChartSelector.js
@@ -9,7 +9,7 @@ export default function ChartSelector({ value, setValue }) {
     <div className="sliderWrapper">
       <p className={`sliderText ${value ? "disabled" : null}`}>AMD</p>
       <label className="switch" for='selector'>
-        <input type="checkbox" checked={value} onChange={handleChange} name='selector'/>
+        <input type="checkbox" checked={value} onChange={handleChange} id='selector'/>
         <span className="slider round"></span>
       </label>
       <p className={`sliderText ${value ? null : "disabled"}`}>NVIDIA</p>
